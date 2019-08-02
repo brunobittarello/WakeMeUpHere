45340d786d74037f0599707f9d2dceb28e6cc83b
Unknown <matheus.rittercamargo@gmail.com>
2019-08-02 02:31:19 -0300

    Activity Navigation
    Getting some error on passing the serializable Object to the next activity

--- app/src/main/java/com/wakemeuphere/AlarmForm.kt
+++ app/src/main/java/com/wakemeuphere/AlarmForm.kt
@@ -3,12 +3,17 @@ package com.wakemeuphere
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_alarm_form.*
 
 class AlarmForm : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_form)
+
+        val alarm = intent.extras?.get("AlarmObject") as Alarm
+        textView.text = alarm.position.toString()
+        //getSerializableExtra("AlarmObject")
     }
 
     fun OnButtonCancelClicked(view: View) {
